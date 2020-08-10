5c4ea4968a2b9dffb39ea4fadb081bb1b07c404f
minkyum <moon.minkyu@gmail.com>
2020-08-10 15:41:29 -0400

    line change

--- run_sh_codes.R
+++ run_sh_codes.R
@@ -5,6 +5,7 @@ setwd('/projectnb/modislc/data/LST/')
 tile <- 'h12v04'
 year <- 2018
 
-system(paste('qsub -V -l h_rt=06:00:00 /usr3/graduate/mkmoon/GitHub/ImageProcess/run_get_MYD11A1.sh ',tile,year,sep=''))        
+system(paste('qsub -V -l h_rt=06:00:00 /usr3/graduate/mkmoon/GitHub/ImageProcess/run_get_MYD11A1.sh ',
+             tile,year,sep=''))        
 
 
